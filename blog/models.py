9e3d0cb9de710c714ae5c6b20bc50fa5b1299977
Vladimir Titov <tvv-vg@yandex.ru>
2024-07-25 17:52:45 +0300

    add dog-string model BlogPost

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,20 @@ NULLABLE = {'blank': True, 'null': True}
 
 
 class BlogPost(models.Model):
+    """
+    Модель публикации.
+
+    Атрибуты:
+        title (CharField): Заголовок публикации.
+        slug (CharField, опционально): Slug публикации.
+        content (TextField): Содержимое публикации.
+        preview (ImageField, опционально): Изображение превью публикации.
+        created_date (DateTimeField): Дата создания публикации.
+        is_premium (BooleanField): Является ли публикация премиум-публикацией.
+        views_count (IntegerField): Количество просмотров публикации.
+        author (ForeignKey): Автор публикации.
+    """
+
     title = models.CharField(max_length=200, verbose_name='Заголовок')
     slug = models.CharField(max_length=200, verbose_name='slug', **NULLABLE)
     content = models.TextField(verbose_name='Содержимое')
