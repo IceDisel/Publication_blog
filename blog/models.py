bd503a0ac9bbc5ce2840e0a797e6c9ff80e75d2c
Vladimir Titov <tvv-vg@yandex.ru>
2024-07-25 17:48:37 +0300

    Создание модели публикации и пользователи

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    slug = models.CharField(max_length=200, verbose_name='slug', **NULLABLE)
+    content = models.TextField(verbose_name='Содержимое')
+    preview = models.ImageField(upload_to='previews/', verbose_name='Изображение', **NULLABLE)
+    created_date = models.DateTimeField(default=timezone.now, verbose_name='Дата создания')
+    is_premium = models.BooleanField(default=False, verbose_name='Премиум публикация')
+    views_count = models.IntegerField(default=0, verbose_name='Количество просмотров')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='Автор')
+
+    class Meta:
+        verbose_name = 'Публикация'
+        verbose_name_plural = 'Публикации'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
--- users/models.py
+++ users/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    """
+    Модель пользователя.
+
+    Атрибуты:
+        phone (PhoneNumberField): Номер телефона пользователя.
+        nickname (CharField): Псевдоним пользователя.
+        city (CharField, опционально): Город пользователя.
+        is_premium (BooleanField): Является ли пользователь премиум-пользователем.
+        is_active (BooleanField): Является ли пользователь активным.
+        token (CharField, опционально): Токен пользователя.
+        payment_session_id (CharField, опционально): ID сессии оплаты пользователя.
+    """
+    username = None
+
+    phone = PhoneNumberField(region='RU', verbose_name='Номер телефона', unique=True)
+    nickname = models.CharField(max_length=200, verbose_name='Псевдоним', unique=True)
+    city = models.CharField(max_length=50, verbose_name='Город', **NULLABLE)
+    is_premium = models.BooleanField(default=False, verbose_name='Премиум пользователь')
+    is_active = models.BooleanField(default=False, verbose_name='Активный пользователь')
+    token = models.CharField(max_length=100, verbose_name='Токен', **NULLABLE)
+    payment_session_id = models.CharField(max_length=300, verbose_name='ID сессии оплаты', **NULLABLE)
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.nickname}'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
