fc4745f5ea4670bbfab6094ff501a0350e345cb0
Vladimir Titov <tvv-vg@yandex.ru>
2024-07-29 23:36:09 +0300

    add CBV

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,132 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import TemplateView, CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
 
-# Create your views here.
+from blog.forms import BlogPostForm
+from blog.models import BlogPost
+from users.models import User
+
+
+class IndexView(TemplateView):
+    """
+    Контролер главной страницы.
+    """
+
+    template_name = "blog/index.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['title'] = 'Многофункциональная платформа цифрового издательства для всех | Joomag'
+        context['total_content'] = BlogPost.objects.all().count()
+        context['premium_content'] = BlogPost.objects.filter(is_premium='True').count()
+        context['all_users'] = User.objects.all().count()
+        context['premium_user'] = User.objects.filter(is_premium='True').count()
+        return context
+
+
+class BlogCreateView(LoginRequiredMixin, CreateView):
+    """
+    Контролер создания публикации,
+    для зарегистрированных пользователей.
+    """
+
+    model = BlogPost
+    form_class = BlogPostForm
+    success_url = reverse_lazy('blog:list')
+    login_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        self.object = form.save()
+        self.object.author = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+
+class BlogListView(LoginRequiredMixin, ListView):
+    """
+    Контролер списка публикаций пользователя,
+    для зарегистрированных пользователей.
+    """
+
+    model = BlogPost
+    login_url = reverse_lazy('users:login')
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return BlogPost.objects.all()
+        else:
+            return BlogPost.objects.filter(author=self.request.user)
+
+
+class BlogDetailView(DetailView):
+    """
+    Контролер просмотра публикации,
+    для зарегистрированных пользователей.
+    """
+
+    model = BlogPost
+    login_url = reverse_lazy('users:login')
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
+
+class BlogUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """
+    Контролер редактирования публикации,
+    для владельцев и администратора.
+    """
+
+    model = BlogPost
+    form_class = BlogPostForm
+    login_url = reverse_lazy('users:login')
+
+    def get_success_url(self):
+        return reverse('blog:view', args=[self.kwargs.get('pk')])
+
+    def test_func(self):
+        content = self.get_object()
+        return self.request.user.is_superuser or self.request.user == content.author
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+
+class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """
+    Контролер удаления публикации,
+    для владельцев и администратора.
+    """
+
+    model = BlogPost
+    success_url = reverse_lazy('blog:list')
+    login_url = reverse_lazy('users:login')
+
+    def test_func(self):
+        content = self.get_object()
+        return self.request.user.is_superuser or self.request.user == content.author
+
+
+class BlogListAllView(ListView):
+    """
+    Контролер списка всех доступных публикаций.
+    """
+
+    model = BlogPost
+    template_name = "blog/blogpost_all_list.html"
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated and self.request.user.is_premium:
+            return BlogPost.objects.all()
+        else:
+            return BlogPost.objects.exclude(is_premium=True)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,121 @@
-from django.shortcuts import render
+import random
+import string
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.shortcuts import redirect, render, get_object_or_404
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, UpdateView
+from smsaero import SmsAeroException
+
+from users.forms import UserRegisterForm, UserForm
+from users.models import User
+from users.servicies import send_sms, create_session
+
+
+class RegisterView(CreateView):
+    """
+    Контролер регистрации пользователя с подтверждением по СМС.
+    """
+
+    model = User
+    form_class = UserRegisterForm
+    template_name = "users/user_register_form.html"
+    success_url = reverse_lazy('users:phone_verify')
+
+    def form_valid(self, form):
+        self.object = form.save()
+        phone = int(self.object.phone.as_e164[1:])
+        token = ''.join(random.choice(string.digits) for i in range(6))
+        self.object.token = token
+        self.object.save()
+        try:
+            send_sms(phone=phone, message=f'Код подтверждения {token}')
+            print('Сообщение отправлено')
+        except SmsAeroException as e:
+            print(f"An error occurred: {e}")
+        return super().form_valid(form)
+
+
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    """
+    Контролер редактирования профиля пользователя.
+    """
+
+    model = User
+    form_class = UserForm
+    success_url = reverse_lazy('users:profile')
+    login_url = reverse_lazy('users:login')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+@login_required
+def payment_create(request):
+    """
+    Контролер для оплаты VIP.
+    """
+
+    user = request.user
+    user.payment_session_id, link = create_session()
+    user.save()
+    context_data = {'link': link}
+    return render(request, 'users/payment_create.html', context_data)
+
+
+@login_required
+def payment_success(request):
+    """
+    Контролер успешной оплаты.
+    """
+
+    user = request.user
+    user.is_premium = True
+    user.save()
+
+    return render(request, 'users/payment_success.html')
+
+
+@login_required
+def payment_cancel(request):
+    """
+    Контролер не успешной оплаты.
+    """
+    return render(request, 'users/payment_cancel.html')
+
+
+def phone_verification(request):
+    """
+    Контролер для верификации номера телефона через код из СМС.
+    """
+    if request.method == 'POST':
+        entered_token = request.POST.get('token', '')
+        nickname = request.POST.get('nickname', '')
+        user = get_object_or_404(User, nickname=nickname)
+        if str(user.token) == str(entered_token):
+            user.is_active = True
+            user.save()
+            return redirect('users:profile')
+        else:
+            return HttpResponse("Код подтверждения неверный. Попробуйте еще раз.")
+    return render(request, 'users/phone_verify.html')
+
+
+def generate_new_password(request):
+    """
+    Контролер создания нового пароля и отправка его по СМС.
+    """
+
+    user = request.user
+    characters = string.ascii_letters + string.digits
+    new_password = ''.join(random.choice(characters) for i in range(12))
+    phone = int(user.phone.as_e164[1:])
+    try:
+        send_sms(phone=phone, message=f'Новый пароль {new_password}')
+    except SmsAeroException as e:
+        print(f"An error occurred: {e}")
+    request.user.set_password(new_password)
+    request.user.save()
+    return redirect(reverse('blog:index'))
