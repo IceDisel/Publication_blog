a8c03512e5fb4cd2b5f7c29dcd48b2d7da24d824
Vladimir Titov <tvv-vg@yandex.ru>
2024-07-29 21:52:08 +0300

    add административная панель

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import BlogPost
+
+
+@admin.register(BlogPost)
+class ContentAdmin(admin.ModelAdmin):
+    """
+    Отображение постов публикаций в админ панели.
+    """
+
+    list_display = ('id', 'title', 'content', 'created_date', 'views_count', 'author', 'is_premium')
